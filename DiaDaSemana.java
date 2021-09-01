0dd40d50e9ed9de558fecb182351ac700b2014c6
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-01 20:16:29 -0300

    Produto em Java que recebe um número de 1 a 7 e diz qual dia da semana é correspondente

--- DiaDaSemana.java
+++ DiaDaSemana.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class DiaDaSemana {
+    //Este programa recebe um número e diz a qual dia da semana ele corresponde
+    public static void main(String[] args) {
+        System.out.println("Entre com um número de 1 a 7 para associar a um dia da semana: ");
+        Scanner teclado = new Scanner(System.in);
+        int numeroDigitado = teclado.nextInt();
+        switch (numeroDigitado){
+            case 1:
+                System.out.println("Esse dia é Domingo");
+                break;
+            case 2:
+                System.out.println("Esse dia é Segunda");
+                break;
+            case 3:
+                System.out.println("Esse dia é Terça");
+                break;
+            case 4:
+                System.out.println("Esse dia é Quarta");
+                break;
+            case 5:
+                System.out.println("Esse dia é Quinta");
+                break;
+            case 6:
+                System.out.println("Esse dia é Sexta");
+                break;
+            case 7:
+                System.out.println("Esse dia é Sábado");
+                break;
+            default:
+                System.out.println("Valor Inválido!");
+        }
+    }
 }
